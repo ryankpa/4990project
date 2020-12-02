ee02d8e61b7c183e2d9edc6eaeda5375de5f19f0
ryan <62574361+ryankpa@users.noreply.github.com>
2020-12-01 20:00:46 -0800

    Add files via upload
    adjusted the partitioning, using MPI scatter call to send parts of the array

--- mpi_practice.py
+++ mpi_practice.py
@@ -7,40 +7,52 @@ comm = MPI.COMM_WORLD
 
 rank = comm.Get_rank()
 size = comm.Get_size()  # 5
-# reading in graph data here
 
 start_time = time.time()
 if rank == 0:   # broadcaster
-    #G = nx.path_graph(200)
-    G = nx.read_edgelist("twitter_combined.txt", create_using=nx.DiGraph)
+    # reading in graph data here
+    # G = nx.read_edgelist("twitter_combined.txt", create_using=nx.DiGraph)
+    G = nx.path_graph(20)
+    all_nodes = list(G.nodes)
+    # divide into chunks
+    nodes_subset = [[] for _ in range(size)]
+    for i, chunk in enumerate(all_nodes):
+        nodes_subset[i % size].append(chunk)
 else:
     G = None
+    nodes_subset = None
+
+# MPI calls
 G = comm.bcast(G, root=0)
-# partitioning
-all_nodes = list(G.nodes)
-N = len(all_nodes)
-nodes_subset = all_nodes[int((rank*N)/size):int(((rank+1)*N)/size)]
+nodes_subset = comm.scatter(nodes_subset, root=0)
+
 print("I am processor", rank)
+print("These are the nodes I have:")
+print(nodes_subset)
+
+# lists for results
 lengths = []
 sums = []
 ccs = []
 
 # doing dijkstra's
-for i in nodes_subset:
+#for i in nodes_subset:
     # print("Source:", i)
-    for j in all_nodes:
-        if nx.has_path(G, i, j):
-            length, path = (nx.bidirectional_dijkstra(G, i, j))
-            lengths.append(length)
-    sums.append(sum(lengths))
-    for j in sums:
-        j = (N-1)/j
-    ccs.append(j)
-    lengths.clear()
+#    for j in all_nodes:
+#        if nx.has_path(G, i, j):
+#            length, path = (nx.bidirectional_dijkstra(G, i, j))
+#            lengths.append(length)
+#    sums.append(sum(lengths))
+#    for j in sums:
+#        if not j == 0:
+#            j = (N-1)/j
+#    ccs.append(j)
+#    lengths.clear()
 
-cc_vals = comm.gather(ccs, root=0)
+# gathering all closeness centrality values
+#cc_vals = comm.gather(ccs, root=0)
 end_time = time.time()
 
 if rank == 0:
-    print(cc_vals, '\n')
+#    print(cc_vals, '\n')
     print("Time taken:", (end_time - start_time))

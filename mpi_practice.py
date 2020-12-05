af7ff549c3a4a3e3e412af4c761bbba616e66fcb
Tyler <35055803+trstamp@users.noreply.github.com>
2020-12-04 16:32:56 -0800

    Update mpi_practice.py
    added dijkstra's

--- mpi_practice.py
+++ mpi_practice.py
@@ -2,6 +2,7 @@ from mpi4py import MPI
 import networkx as nx
 import time
 import numpy as np
+from fibheap import *
 
 
 comm = MPI.COMM_WORLD
@@ -13,7 +14,7 @@ start_time = time.time()
 if rank == 0:   # broadcaster
     # reading in graph data here
     # G = nx.read_edgelist("twitter_combined.txt", create_using=nx.DiGraph)
-    G = nx.path_graph(20)
+    G = nx.path_graph(5)
     # convert G to numpy matrix
     M = nx.to_numpy_matrix(G)
     all_nodes = list(G.nodes)
@@ -33,12 +34,33 @@ print("I am processor", rank)
 print("These are the nodes I have:")
 print(nodes_subset)
 
+#
+dist = []
+heap1 = makefheap()
+
 # lists for results
 lengths = []
 sums = []
 ccs = []
 
-# closeness centrality calculations here
+# closeness centrality calculations here / dijkstra's
+for source in nodes_subset:
+    for v in all_nodes:
+        dist[v] = float("inf")
+        fheappush(heap1, (dist[v], v))
+    dist[source] = 0
+    fheappush(heap1, dist[source], source)
+    while heap1 is not None:
+        u = fheappop(heap1)
+        for v in M:
+            if v in heap1:
+                alt = dist[u] + M[u, v]
+                if alt < dist[v]:
+                    dist[v] = alt
+    print(dist)
+
+
+
 
 # gathering all closeness centrality values
 cc_vals = comm.gather(ccs, root=0)
